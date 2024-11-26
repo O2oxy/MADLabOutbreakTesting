ae02556cd88c16a1aa1a1431f3a55f9a078c37bb
O2oxy <ruiborges2003@gmail.com>
2024-11-26 10:08:18 +0000

    Animações + Controladores
    Adicionar animações ao player
    Colocação apropriada das mãos na arma
    Colisões Fix
    Controlador de movimento do player - Refeito
    Controlador da camara XYZ - em progresso

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class PlayerLocomotion : MonoBehaviour
 {
+    Animator animator;
+    Vector2 input;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("InputX", input.x);
+        animator.SetFloat("InputY", input.y);
+    }
 }
